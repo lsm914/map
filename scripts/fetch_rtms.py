d3e6006c016ab0d1c98b39e401c4971f53678d73
lsm914 <87294074+lsm914@users.noreply.github.com>
2025-08-12 16:56:58 +0900

    Update fetch_rtms.py

--- scripts/fetch_rtms.py
+++ scripts/fetch_rtms.py
@@ -32,8 +32,8 @@ date_list = [(today - relativedelta(months=i)).strftime("%Y%m") for i in range(1
 date_list = sorted(set(date_list))  # 오래→최근 순
 
 # ---------- 2) 기준 테이블 ----------
-lawd_df = pd.read_csv(REF_DIR / "lawd_codes.csv")  # 컬럼: lawd_cd(5자리), sido_nm, sigungu_nm
-cent_df = pd.read_csv(REF_DIR / "sigungu_centroids.csv")  # 컬럼: lawd_cd, lat, lng, region_group(수도권/특광역시/지방)
+lawd_df = pd.read_csv("lawd_codes.csv")  # 컬럼: lawd_cd(5자리), sido_nm, sigungu_nm
+cent_df = pd.read_csv("sigungu_centroids.csv")  # 컬럼: lawd_cd, lat, lng, region_group(수도권/특광역시/지방)
 
 lawd_list = lawd_df["lawd_cd"].astype(str).str.zfill(5).unique().tolist()
 
